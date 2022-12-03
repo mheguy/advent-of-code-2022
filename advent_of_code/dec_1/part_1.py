1048cd54ae44cf2e156df2ffc9d7c0e5c55eae0b
mheguy <mheguy@stingray.com>
2022-12-02 22:39:16 -0500

    Add dec_1 solution

---
+++ advent_of_code/dec_1/part_1.py
@@ -0,0 +1,18 @@
+from shared_lib.resources import INPUT_FOLDER
+
+
+def get_elves() -> list[int]:
+    input_file = INPUT_FOLDER / "dec_1" / "input.txt"
+    input_text = input_file.read_text("UTF-8")
+    elves_as_text = input_text.split("\n\n")
+
+    elves = []
+    for elf_text in elves_as_text:
+        calorie_values_as_text = elf_text.split("\n")
+        elves.append(sum(int(calorie_value_str) for calorie_value_str in calorie_values_as_text))
+    return elves
+
+
+if __name__ == "__main__":
+    elven_calorie_counts = get_elves()
+    print(max(elven_calorie_counts))
