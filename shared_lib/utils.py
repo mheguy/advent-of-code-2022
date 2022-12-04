026dce7d406ec28e1c6183c7de0977188554feb3
Matthew <10162554+plague006@users.noreply.github.com>
2022-12-04 20:50:15 +0000

    Filter empty lines

--- shared_lib/utils.py
+++ shared_lib/utils.py
@@ -1,7 +1,14 @@
+from typing import Iterable
+
 from shared_lib.resources import INPUT_FOLDER
 
 
-def get_input_file_lines() -> list[str]:
+def get_input_file_lines() -> Iterable[str]:
     input_file = INPUT_FOLDER / "input"
     input_text = input_file.read_text("UTF-8")
-    return input_text.split("\n")
+    lines = input_text.split("\n")
+    for line in lines:
+        if line:
+            yield line
+        else:
+            return
