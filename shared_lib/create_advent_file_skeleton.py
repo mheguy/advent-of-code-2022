12770bed6b90739e7cb816d3adcc10de1eb3e6c4
mheguy <mheguy@stingray.com>
2022-12-06 00:53:09 -0500

    Update skeleton

--- shared_lib/create_advent_file_skeleton.py
+++ shared_lib/create_advent_file_skeleton.py
@@ -2,11 +2,20 @@ from pathlib import Path
 
 root_folder = Path(__file__).parent
 
-for i in range(25):
+for i in range(5, 25):
     i += 1
-    day_folder = root_folder / f"dec_{i}"
+    day_folder = root_folder / f"dec_{i:02d}"
     day_folder.mkdir(exist_ok=True)
 
     for part in range(2):
         part_file = day_folder / f"part_{part + 1}.py"
-        part_file.touch(exist_ok=True)
+        part_file.write_text(
+            """def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
+"""
+        )
+        # part_file.touch(exist_ok=True)
