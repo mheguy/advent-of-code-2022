d185f4f2f0b5bde9e13ed97db4fbad9704e64d3f
mheguy <mheguy@stingray.com>
2022-12-06 01:09:26 -0500

    Add dec_06 solution

--- advent_of_code/dec_06/part_1.py
+++ advent_of_code/dec_06/part_1.py
@@ -1,6 +1,18 @@
-def main():
-    ...
+from collections import deque
+
+from shared_lib.utils import get_input_file_text
+
+
+def main(cypher_length):
+    input_text = get_input_file_text()
+    cypher = deque([], cypher_length)
+
+    for pos, char in enumerate(input_text, start=1):
+        cypher.append(char)
+        if len(cypher) == cypher_length and len(set(cypher)) == cypher_length:
+            print(pos)
+            break
 
 
 if __name__ == "__main__":
-    main()
+    main(4)
--- advent_of_code/dec_06/part_2.py
+++ advent_of_code/dec_06/part_2.py
@@ -1,6 +1,4 @@
-def main():
-    ...
-
+from advent_of_code.dec_06.part_1 import main as part_1
 
 if __name__ == "__main__":
-    main()
+    part_1(14)
